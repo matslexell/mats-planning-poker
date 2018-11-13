f6ecfe5072beb907f3771e9e55b2805f8eecdb4a
Mats Lexell <mats@axesslab.com>
2018-11-13 20:01:22 +0100

    UI done for meeting page, loaded with fake data

--- src/main/webapp/app/meeting-page/meeting-page.component.ts
+++ src/main/webapp/app/meeting-page/meeting-page.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { Meeting } from 'app/shared/model/meeting.model';
 
 @Component({
     selector: 'jhi-meeting-page',
-    templateUrl: './meeting-page.component.html'
+    templateUrl: './meeting-page.component.html',
+    styleUrls: ['meeting-page.scss']
 })
 export class MeetingPageComponent implements OnInit {
+    planningPokerValues = ['0', '1/2', '1', '2', '3', '5', '8', '13'];
+    meeting: Meeting = {};
+    url: String;
+    myVote: String = '';
+    results: any;
+
     constructor(private activatedRoute: ActivatedRoute, private location: Location, private router: Router) {}
 
     public ngOnInit(): void {
+        this.meeting = {
+            name: 'my meeting',
+            participants: [
+                { name: 'mats', vote: '1/2' },
+                { name: 'ida', vote: '5' },
+                { name: 'axel', vote: '5' },
+                { name: 'martin', vote: 'no vote' },
+                { name: 'robert', vote: '1' }
+            ],
+            uuid: '8kvg3mdslm3v'
+        };
+
+        this.results = this.calculateResult(this.meeting, this.planningPokerValues);
+
+        console.log('Results', this.results);
+
         const meetingId = this.activatedRoute.snapshot.paramMap.get('meetingId');
 
         const url =
@@ -20,6 +44,20 @@ export class MeetingPageComponent implements OnInit {
         this.activatedRoute.queryParams.subscribe(params => {
             console.log('Participant Name:', params.participantName);
             this.location.go(url); // Set url without rerouting
+            this.url = window.location.href;
         });
+    }
+
+    private calculateResult(meeting: Meeting, planningPokerValues: String[]): { value: String; count: Number }[] {
+        return planningPokerValues.map(planningPokerValue => {
+            return {
+                value: planningPokerValue,
+                count: meeting.participants.map(participant => participant.vote).filter(vote => vote === planningPokerValue).length
+            };
+        });
+    }
+
+    public submitVote() {
+        console.log('submitVote', this.myVote);
     }
 }
--- src/main/webapp/app/meeting-page/meeting-page.module.ts
+++ src/main/webapp/app/meeting-page/meeting-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MeetingPageComponent } from './meeting-page.component';
+import { FormsModule } from '@angular/forms';
+import { MatsPlanningPokerSharedModule } from 'app/shared';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, MatsPlanningPokerSharedModule, RouterModule, FormsModule],
     declarations: [MeetingPageComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     entryComponents: []
